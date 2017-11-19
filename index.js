b56169df8d916595ab2d87b0fd8189679ce31b90
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-19 15:37:25 +0100

    update port for heroku

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+let port = process.env.PORT || 3000;
 
-console.log('STARTING SERVER 🌈 🦄 ✨');
+console.log("STARTING SERVER 🌈 🦄 ✨");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', socket => {
-  console.log('CLIENT CONNECTED 💃 ', socket.id);
+io.on("connection", socket => {
+  console.log("CLIENT CONNECTED 💃 ", socket.id);
 
-  socket.on('stateChanged', msg => {
-    console.log('new program: ' + msg);
+  socket.on("stateChanged", msg => {
+    console.log("new program: " + msg);
     io.emit("updateState", msg);
   });
 
-  socket.on('disconnect', () => {
-    console.log('client disconnected 💩');
+  socket.on("disconnect", () => {
+    console.log("client disconnected 💩");
 
     socket.disconnect();
   });
 });
 
-server.listen(3000, () => {
-  console.log('The server is running');
+server.listen(port, () => {
+  console.log("The server is running");
 });
