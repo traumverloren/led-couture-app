9ddbcd023cc47cc9e7e69be936a5d6c8a702d3d4
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-21 15:03:55 +0100

    add svg background, add static assets, favico, add middleware for sass transpiling

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+var path = require('path');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-let port = process.env.PORT || 3002;
+const sassMiddleware = require('node-sass-middleware');
+let port = process.env.PORT || 8000;
 
 console.log('STARTING SERVER 🌈 🦄 ✨');
+
+app.use(
+  sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/public'
+  })
+);
+// Note: you must place sass-middleware *before* `express.static` or else it will
+// not work.
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
