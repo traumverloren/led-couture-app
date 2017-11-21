79bda03b0c77e9e0ffd9fcac091f1524d1894ffb
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-21 13:06:08 +0100

    update port default

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const app = require("express")();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-let port = process.env.PORT || 8080;
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+let port = process.env.PORT || 3002;
 
-console.log("STARTING SERVER 🌈 🦄 ✨");
+console.log('STARTING SERVER 🌈 🦄 ✨');
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", socket => {
-  console.log("CLIENT CONNECTED 💃 ", socket.id);
+io.on('connection', socket => {
+  console.log('CLIENT CONNECTED 💃 ', socket.id);
 
-  socket.on("stateChanged", msg => {
-    console.log("new program: " + msg);
-    io.emit("updateState", msg);
+  socket.on('stateChanged', msg => {
+    console.log('new program: ' + msg);
+    io.emit('updateState', msg);
   });
 
-  socket.on("disconnect", () => {
-    console.log("client disconnected 💩");
+  socket.on('disconnect', () => {
+    console.log('client disconnected 💩');
 
     socket.disconnect();
   });
 });
 
 server.listen(port, () => {
-  console.log("Express app listening on port:", port);
+  console.log('Express app listening on port:', port);
 });
