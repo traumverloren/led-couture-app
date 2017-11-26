53dca22850f73af8c263820e5300cd4ef99f5e10
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-26 20:28:56 +0100

    send msg when client connected

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var path = require('path');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const sassMiddleware = require('node-sass-middleware');
+var path = require("path");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const sassMiddleware = require("node-sass-middleware");
 let port = process.env.PORT || 8000;
 
-console.log('STARTING SERVER 🌈 🦄 ✨');
+console.log("STARTING SERVER 🌈 🦄 ✨");
 
 app.use(
   sassMiddleware({
     /* Options */
-    src: path.join(__dirname, 'sass'),
-    dest: path.join(__dirname, 'public'),
+    src: path.join(__dirname, "sass"),
+    dest: path.join(__dirname, "public"),
     debug: true,
-    outputStyle: 'compressed',
-    prefix: '/public'
+    outputStyle: "compressed",
+    prefix: "/public"
   })
 );
 // Note: you must place sass-middleware *before* `express.static` or else it will
 // not work.
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', socket => {
-  console.log('CLIENT CONNECTED 💃 ', socket.id);
+io.on("connection", socket => {
+  console.log("CLIENT CONNECTED 💃 ", socket.id);
+  io.emit("clientConnected", socket.id);
 
-  socket.on('stateChanged', msg => {
+  socket.on("stateChanged", msg => {
     io.emit(msg.program, msg.colors);
   });
 
-  socket.on('disconnect', () => {
-    console.log('client disconnected 💩');
+  socket.on("disconnect", () => {
+    console.log("client disconnected 💩");
 
     socket.disconnect();
   });
 });
 
 server.listen(port, () => {
-  console.log('Express app listening on port:', port);
+  console.log("Express app listening on port:", port);
 });
