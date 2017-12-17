daf894790f71422066522db40b64e1ca3b09cf84
Stephanie Nemeth <traumverloren@gmail.com>
2017-12-17 17:08:06 +0100

    shiftr works

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// // Serve static assets
+// app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+
+// // Always return the main index.html, so react-router render the route in the client
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
+
 server.listen(port, () => {
   console.log("Express app listening on port:", port);
 });
