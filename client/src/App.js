3cab335a49ab8fd2efe5c7c8f1c6dfbd2701eae9
Stephanie Nemeth <traumverloren@gmail.com>
2017-12-18 21:15:36 +0100

    WIP transition to react client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import umbrella from "./assets/animated-pixel-umbrella.gif";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  margin: 0 auto;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Heading = styled.h1`
+  font-family: "Pacifico";
+  padding: 10px;
+  position: relative;
+  text-align: center;
+  margin: 0;
+  transform: translateX(-60px);
+  font-size: 28px;
+
+  @media (min-width: 420px) {
+    font-size: 36px;
+    transform: translateX(-80px);
+  }
+
+  @media (min-width: 769px) {
+    font-size: 60px;
+    transform: translateX(-120px);
+  }
+
+  @media (min-width: 1440px) {
+    transform: translateX(-140px);
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Main>
+          <Heading>Light My</Heading>
+          <img src={umbrella} />
+        </Main>
+      </Container>
     );
   }
 }
